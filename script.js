685f75f57078ec3dbf196852739508fda5c41e36
Ritik Pathak <130272666+imrikkie@users.noreply.github.com>
2024-08-23 14:53:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,114 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'Which NIT is best?',
+    answers: [
+      { text: 'NITH', correct: false },
+      { text: 'NITRR', correct: true },
+      { text: 'NITR', correct: false },
+      { text: 'NITW', correct: false }
+    ]
+  },
+  {
+    question: 'you live in which country?',
+    answers: [
+      { text: 'India', correct: true },
+      { text: 'Japan', correct: false },
+      { text: 'Bangladesh', correct: false },
+      { text: 'USA', correct: false }
+    ]
+  },
+  {
+    question: 'How was the Budget of financial year 2024-25?',
+    answers: [
+      { text: 'I was good', correct: false },
+      { text: 'Not Satisfied', correct: true },
+      { text: 'Just OKK', correct: false },
+      { text: 'who cares', correct: false }
+    ]
+  },
+  {
+    question: 'Are you enjoying quiz?',
+    answers: [
+      { text: 'YES', correct: false },
+      { text: 'NO', correct: true }
+    ]
+  }
+]
